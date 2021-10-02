3b96456bdf1f5019f5c7b19e844a6f3e53e86f90
Shimpei Otsubo <potsbo@gmail.com>
2021-10-03 01:58:18 +0900

    antialiasing

--- vec3.hpp
+++ vec3.hpp
@@ -15,6 +15,13 @@ public:
   double y() const { return e[1]; }
   double z() const { return e[2]; }
 
+  vec3 &operator+=(const vec3 &v) {
+    e[0] += v.e[0];
+    e[1] += v.e[1];
+    e[2] += v.e[2];
+    return *this;
+  }
+
   double length() const { return sqrt(length_squared()); }
 
   double length_squared() const {
