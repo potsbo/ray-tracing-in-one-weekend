b3e3467463d5c2cc829006e425a4c8d7cd62f6f0
Shimpei Otsubo <potsbo@gmail.com>
2021-10-02 23:50:25 +0900

    Visualize normal vectors

--- ray.hpp
+++ ray.hpp
@@ -7,9 +7,10 @@ class ray {
 public:
   ray(const point3 &origin, const vec3 &direction)
       : orig(origin), dir(direction) {}
+  vec3 at(double t) const { return orig + t * dir; }
 
-	vec3 direction() const { return dir; }
-	vec3 origin() const { return orig; }
+  vec3 direction() const { return dir; }
+  vec3 origin() const { return orig; }
 
 public:
   point3 orig;
