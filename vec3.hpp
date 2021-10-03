37e153dda25b67c13cac971b4da21a6723183ce6
Shimpei Otsubo <potsbo@gmail.com>
2021-10-03 17:36:33 +0900

    Format

--- vec3.hpp
+++ vec3.hpp
@@ -52,7 +52,7 @@ inline vec3 operator+(vec3 v1, vec3 v2) {
 inline vec3 operator-(vec3 v1, vec3 v2) { return v1 + (-v2); }
 
 inline vec3 operator*(const vec3 &u, const vec3 &v) {
-    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+  return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
 inline vec3 operator*(double t, vec3 v) {
@@ -79,8 +79,8 @@ inline vec3 random_unit_vector() {
   return unit_vector(random_in_unit_sphere());
 }
 
-inline vec3 reflect(const vec3& v, const vec3& n) {
-    return v - 2*dot(v,n)*n;
+inline vec3 reflect(const vec3 &v, const vec3 &n) {
+  return v - 2 * dot(v, n) * n;
 }
 
 using point3 = vec3;
