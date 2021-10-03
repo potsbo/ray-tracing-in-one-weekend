05cd2a2e575ab0317cb0603c345a30cb769fcc17
Shimpei Otsubo <potsbo@gmail.com>
2021-10-03 17:08:10 +0900

    Fuzz

--- material.hpp
+++ material.hpp
@@ -32,18 +32,19 @@ public:
 
 class metal : public material {
 public:
-  metal(const color &a) : albedo(a) {}
+  metal(const color &a, double fuzz) : albedo(a), fuzz(fuzz) {}
 
   virtual bool scatter(const ray &r_in, const hit_record &rec,
                        color &attenuation, ray &scattered) const override {
-    vec3 refleted = reflect(unit_vector(r_in.direction()), rec.normal);
-    scattered = ray(rec.p, refleted);
+    vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+    scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
     attenuation = albedo;
     return (dot(scattered.direction(), rec.normal) > 0);
   }
 
 public:
   color albedo;
+  double fuzz;
 };
 
 #endif
